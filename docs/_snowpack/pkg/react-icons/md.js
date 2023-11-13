94017720e3e29e84855cdd9363cf65d799b1a2ba
jwilson9567 <josephwilson9567@example.com>
2023-11-12 21:39:47 -0600

    added the correct files

---
+++ docs/_snowpack/pkg/react-icons/md.js
@@ -0,0 +1,14 @@
+import { e as esm } from '../common/index-f4e90b0c.js';
+import '../common/index-bd03b9e6.js';
+import '../common/_commonjsHelpers-8c19dec8.js';
+
+// THIS FILE IS AUTO GENERATED
+var GenIcon = esm.GenIcon;
+var MdExpandLess = function MdExpandLess (props) {
+  return GenIcon({"tag":"svg","attr":{"viewBox":"0 0 24 24"},"child":[{"tag":"path","attr":{"fill":"none","d":"M0 0h24v24H0z"}},{"tag":"path","attr":{"d":"M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"}}]})(props);
+};
+var MdExpandMore = function MdExpandMore (props) {
+  return GenIcon({"tag":"svg","attr":{"viewBox":"0 0 24 24"},"child":[{"tag":"path","attr":{"fill":"none","d":"M0 0h24v24H0z"}},{"tag":"path","attr":{"d":"M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"}}]})(props);
+};
+
+export { MdExpandLess, MdExpandMore };
