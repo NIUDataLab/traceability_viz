49646bb94f3d5d467c202e3e0deba38371ab8fc3
jwilson9567 <josephwilson9567@example.com>
2023-11-14 03:33:22 -0600

    resetting docs folder

--- docs/dist/index_2.js
+++
@@ -1,5 +0,0 @@
-import React from "../_snowpack/pkg/react.js";
-import ReactDOM from "../_snowpack/pkg/react-dom.js";
-import DescriptionPanel from "./description_panel.js";
-import "./styles.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(DescriptionPanel, null)), document.getElementById("root"));
