4b5d6fd5ad7d83e78ed8c738928b814ca33da6d5
jwilson9567 <josephwilson9567@example.com>
2023-11-14 02:57:32 -0600

    ipdated index.js

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -61,7 +61,7 @@ document.getElementById("data-select")?.addEventListener("change", (event) => {
     alert("Please choose a data set to work with!");
     return;
   }
-  fetch("http://jwilson9567.pythonanywhere.com/different.json", {
+  fetch("https://jwilson9567.pythonanywhere.com/different.json", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
@@ -74,7 +74,7 @@ document.getElementById("data-select")?.addEventListener("change", (event) => {
       single_node_graph_view.addNode(node, {label: node});
     }
   });
-  fetch("http://jwilson9567.pythonanywhere.com/all_node_description_data.json", {
+  fetch("https://jwilson9567.pythonanywhere.com/all_node_description_data.json", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
